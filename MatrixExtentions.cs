06864fb1d4eb95bafc135b9a09baf0cb538bdf94
Halepaa <kovaliknazario@gmail.com>
2022-10-18 17:20:59 +0300

    initial commit

---
+++ MatrixExtentions.cs
@@ -0,0 +1,15 @@
+﻿namespace test;
+
+public static class MatrixExtentions
+{
+    public static int[] GetColumn(this int[][] matrix, int column)
+    {
+        var values = new List<int>(matrix.Length);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            values.Add(matrix[i][column]);
+        }
+
+        return values.ToArray();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,177 @@
+﻿using test;
+
+var matrix = Generate(10);
+Print(matrix);
+
+DeleteAllSeries(matrix);
+Print(matrix);
+void DeleteAllSeries(int[][] matrix)
+{
+    while (true)
+    {
+        if (ColumnSeriesAreNotEmpty(matrix))
+        {
+            HandleColumns(matrix);
+            continue;
+        }
+
+        if (RowSeriesAreNotEmpty(matrix))
+        {
+            HandleRows(matrix);
+            continue;
+        }
+
+        break;
+    }
+}
+
+void HandleColumns(int[][] matrix)
+{
+    for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+    {
+        var series = GetSeries(matrix.GetColumn(i));
+        if (series.Any() == false) // I hate using !
+            continue;
+
+        MoveColumn(matrix, series, column: i);
+    }
+}
+
+void MoveColumn(int[][] matrix, IEnumerable<(int Start, int Count)> series, int column)
+{
+    foreach (var seria in series)
+    {
+        MoveColumnByOneSeria(matrix, seria, column);
+    }
+}
+
+void MoveColumnByOneSeria(int[][] matrix, (int Start, int Count) seria, int column)
+{
+    for (int i = seria.Start - 1; i >= 0; i--)
+    {
+        matrix[i][column] = matrix[i + seria.Count][column];
+    }
+
+    for (int i = 0; i < seria.Count; i++)
+    {
+        matrix[i][column] = Random.Shared.Next(0, 2);
+    }
+}
+
+void MoveRowByOneSeria(int[][] matrix, (int Start, int Count) seria, int row)
+{
+    for (int i = row; i >= 0; i--)
+    {
+        for (int j = seria.Start; j < seria.Start + seria.Count; j++)
+        {
+            if (i != 0)
+            {
+                matrix[i][j] = matrix[i - 1][j];
+            }
+            else
+            {
+                matrix[i][j] = Random.Shared.Next(0, 2);
+            }
+        } 
+    }
+}
+void HandleRows(int[][] matrix)
+{
+    for (int i = matrix.GetLength(0) - 1; i >= 0; i--)
+    {
+        var series = GetSeries(matrix[i]);
+        if (series.Any() == false)
+        {
+            continue;
+        }
+
+        MoveRow(matrix, series, row: i);
+    }
+}
+
+bool RowSeriesAreNotEmpty(int[][] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        var series = GetSeries(matrix[i]).ToList();
+        if (series.Any())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool ColumnSeriesAreNotEmpty(int[][] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        var series = GetSeries(matrix.GetColumn(i));
+        if (series.Any())
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+void MoveRow(int[][] matrix, IEnumerable<(int Start, int Count)> series, int row)
+{
+    foreach (var item in series)
+    {
+        MoveRowByOneSeria(matrix, item, row);
+    }
+}
+
+
+IEnumerable<(int Start, int Count)> GetSeries(int[] arr)
+{
+    for (int i = 0; i < arr.Length;)
+    {
+        var element = arr[i];
+        var count = arr[i..].TakeWhile(x => element == x).Count();
+        if (count >= 3)
+        {
+            yield return (i, count);
+        }
+
+        i += count;
+    }  
+}
+
+int[][] Generate(int size)
+{
+    var matrix = new int[size][];
+    for (int i = 0; i < size; i++)
+    {
+        matrix[i] = new int[size];
+    }
+    var rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrix[i][j] = rnd.Next(0, 2);
+        }
+    }
+
+    return matrix;
+}
+
+void Print(int[][] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        for (int j = 0; j < matrix[i].Length; j++)
+        {
+            Console.Write(matrix[i][j] + "\t");
+        }
+
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+}
